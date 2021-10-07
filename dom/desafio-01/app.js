14bbbb17f20d73984e8d3ff2275e5266c15ce415
Vanessa Elizabeth Piccoli <vanepiccoli7@gmail.com>
2021-10-06 21:05:43 -0300

    Curso dia 06/10

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Vanessa Piccoli',
+        idade: 24,
+        imagem: 'https://conteudo.imguol.com.br/c/entretenimento/16/2017/06/27/naruto-1498593686428_v2_450x337.png'
+    }
+})
